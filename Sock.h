c063b5bb11033a438ef5c6d31254fcbcb439f2ba
theJulman <name1.nachname2@hotmail.de>
2018-07-08 17:00:51 +0200

    Add files via upload

---
+++ Sock.h
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Networking.h"
+#include "Sock.generated.h"
+
+UCLASS()
+class TCP_API ASock : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASock();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* mesh;
+
+	FSocket* ListenerSocket;
+	FSocket* ConnectionSocket;
+	FIPv4Endpoint RemoteAddressForConnection;
+
+	bool StartTCPReceiver(
+		const FString& YourChosenSocketName,
+		const FString& TheIP,
+		const int32 ThePort
+	);
+
+	FSocket* CreateTCPConnectionListener(
+		const FString& YourChosenSocketName,
+		const FString& TheIP,
+		const int32 ThePort,
+		const int32 ReceiveBufferSize = 2 * 1024 * 1024
+	);
+
+	//Timer functions, could be threads
+	void TCPConnectionListener(); 	//can thread this eventually
+	void TCPSocketListener();		//can thread this eventually
+
+
+									//Format String IP4 to number array
+	bool FormatIP4ToNumber(const FString& TheIP, uint8(&Out)[4]);
+
+	//Rama's StringFromBinaryArray
+	FString StringFromBinaryArray(const TArray<uint8>& BinaryArray);
+
+	FTimerHandle ConnectionListenTimer;
+	FTimerHandle SocketListenTimer;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "REKT")
+	void Avatar(const FString& RektString);
+	
+};
